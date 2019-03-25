00a790cb0b79d5fa18a5195e27b3664637ea2eb5
Lizzy Brooks <lizzy.brooks@gmail.com>
2019-03-25 15:56:25 -0700

    Add files via upload

---
+++ array-push-object/sketch.js
@@ -0,0 +1,51 @@
+
+//create an empty array called balls
+let balls = [];
+
+function setup() {
+  createCanvas(800, 400);
+  for(let i=0;i<9;i++){
+      let  b = new Ball(random(0,100), random(0,height));
+      balls.push(b);
+      console.log(balls);
+
+  }
+
+
+}
+
+function draw(){
+	background(220);
+
+//	draw all the balls in that array
+	for (let i = 0; i < balls.length; i++) {
+	    balls[i].drawBall();
+        balls[i].moveBall();
+	  }
+}
+
+function keyPressed(){ //every time you push a key, make a new ball from the ball class and add it to the balls array
+
+}
+
+//ball class from which to create new balls with similar properties.
+class Ball {
+
+	constructor(x,y){ //every ball needs an x value and a y value
+		    this.x = x;
+    		this.y = y;
+	}
+
+	drawBall(){  // draw a ball on the screen at x,y
+    		stroke(0);
+    		fill("red");
+		    ellipse(this.x,this.y,10,10);
+	}
+
+	moveBall(){ //update the location of the ball, so it moves across the screen
+		this.x = this.x+2;
+		this.y = this.y+.5;
+	}
+
+
+}
