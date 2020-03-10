a205da29f34ec36652d486fac9f8db09320891d4
Skittlemax <max@voidstar.net>
2020-03-10 10:30:51 -0700

    Made the ball a hypercube and added 2 extra hypercubes

--- 1-objects-classes/sketch.js
+++ 1-objects-classes/sketch.js
@@ -1,11 +1,15 @@
 //create a variable to hold one ball
 let b;
 let anotherBall;
+let thirdBall;
+let fourthBall;
 
 function setup() {
   createCanvas(800, 400);
   b = new Ball(0, 100,"red"); //make a new ball from the Ball class and call it b.
-  anotherBall = new Ball(200,20,"green");
+  anotherBall = new Ball(200,20,"yellow");
+  thirdBall = new Ball(100,150, "orange");
+  fourthBall = new Ball(200,300, "purple");
 }
 
 
@@ -15,6 +19,10 @@ function draw(){
     b.moveBall(); //move the ball called b (go look in the Ball class for the moveBall function)
     anotherBall.drawBall();
     anotherBall.moveBall();
+    thirdBall.drawBall();
+    thirdBall.moveBall();
+    fourthBall.drawBall();
+    fourthBall.moveBall();
 
 }
 
@@ -30,10 +38,50 @@ class Ball {
 	drawBall(){  // draw a ball on the screen at x,y
     		stroke(0);
     		fill(this.color);
-		    ellipse(this.x,this.y,10,10);
+		    hypercube(this.x,this.y,10,10);
 	}
 	moveBall(){ //update the location of the ball, so it moves across the screen
 		this.x = this.x+2;
 		this.y = this.y+.5;
 	}
+}
+  function hypercube_1(x,y) {
+  push();
+  noFill();
+  line(x+125-140,y+105-140,x+125-140,y+170-140);
+  line(x+105-140,y+125-140,x+170-140,y+125-140);
+  line(x+105-140,y+150-140,x+170-140,y+150-140);
+  line(x+150-140,y+170-140,x+150-140,y+105-140);
+  line(x+150-140,y+105-140,x+105-140,y+150-140);
+  line(x+105-140,y+125-140,x+150-140,y+170-140);
+  line(x+125-140,y+170-140,x+170-140,y+125-140);
+  line(x+170-140,y+150-140,x+125-140,y+105-140);
+  line(x+105-140,y+125-140,x+105-140,y+60-140);
+  line(x+105-140,y+60-140,x+170-140,y+60-140);
+  line(x+170-140,y+60-140,x+170-140,y+125-140);
+  line(x+150-140,y+105-140,x+215-140,y+105-140);
+  line(x+215-140,y+105-140,x+215-140,y+170-140);
+  line(x+215-140,y+170-140,x+150-140,y+170-140);
+  line(x+170-140,y+150-140,x+170-140,y+215-140);
+  line(x+170-140,y+215-140,x+105-140,y+215-140);
+  line(x+105-140,y+215-140,x+105-140,y+150-140);
+  line(x+125-140,y+170-140,x+60-140,y+170-140);
+  line(x+60-140,y+170-140,x+60-140,y+105-140);
+  line(x+60-140,y+105-140,x+125-140,y+105-140);
+  line(x+105-140,y+150-140,x+60-140,y+105-140);
+  line(x+60-140,y+105-140,x+105-140,y+60-140);
+  line(x+105-140,y+60-140,x+150-140,y+105-140);
+  line(x+125-140,y+105-140,x+170-140,y+60-140);
+  line(x+170-140,y+60-140,x+215-140,y+105-140);
+  line(x+215-140,y+105-140,x+170-140,y+150-140);
+  line(x+170-140,y+125-140,x+170-140,y+170-140);
+  line(x+215-140,y+170-140,x+170-140,y+215-140);
+  line(x+170-140,y+215-140,x+125-140,y+170-140);
+  line(x+150-140,y+170-140,x+105-140,y+215-140);
+  line(x+105-140,y+215-140,x+60-140,y+170-140);
+  line(x+60-140,y+170-140,x+105-140,y+125-140);
+  pop();
+}
+function hypercube(x,y) {
+hypercube_1(x+2.5,y+2.5);
 }
