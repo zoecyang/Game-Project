28c17e797868e04419a243b6eb2fdc6dc15b8303
dsass <dsass@users.noreply.github.com>
2020-11-12 08:28:55 -0800

    Update sketch.js

--- empty-example/sketch.js
+++ empty-example/sketch.js
@@ -1,13 +1,8 @@
 
 function setup() {
-  createCanvas(700, 600);
+  createCanvas(600, 600);
 }
 
 function draw(){
-	background(220,100,3);
-  fill("black")
-  ellipse(100,100,40,50);
-  fill("blue")
-  rect(200,200,100,150)
-
+	background(220);
 }
